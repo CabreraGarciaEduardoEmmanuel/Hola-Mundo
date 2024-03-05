7b63d0ca126f89b000e43f36b88dab546e7b3f5e
CabreraGarciaEduardoEmmanuel <143464606+CabreraGarciaEduardoEmmanuel@users.noreply.github.com>
2024-03-05 01:49:24 +0000

    Version5

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
 #include <Mascota.hpp>
@@ -7,22 +8,36 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    Dibujo dibujo("./assets/wilson.txt");
-    dibujo.Dibujar();
+    list<Dibujo> dibujos;
+
+
+    Dibujo dibujo1("./assets/wilson.txt");
+    dibujo1.Dibujar();
     Dibujo dibujo2("./assets/dragon.txt");
     dibujo2.Dibujar();
 
-    Mascota m1;
-    Mascota m2;
-    Mascota m3;
+    // dibujos.push_front(dibujo1);
+    // dibujos.push_front(dibujo2);
+
+
+    Mascota m1,m2,m3;
+
+    list<Mascota> mascotas;
+    mascotas.push_back(m1);
+    mascotas.push_back(m2);
+    mascotas.push_back(m3);
 
     Alimento alimento(5);
+    for (auto &&mascotaActual : mascotas)
+    {
+        mascotaActual.Comer(alimento);
+    }
+    
+    for (auto &&mascotaActual : mascotas)
+    {
+    cout<<"Mascota " << mascotaActual.LeerEnergia() <<endl;     
+    }
 
-    m1.Comer(alimento);
-    m2.Comer(alimento);
-    m3.Comer(alimento);
-
-    cout<<"Mascota " << m1.LeerEnergia() <<endl;
     return 0;
 }
 
